9023e433af4fdfec915d15d13b7e145e23e47aec
seblast31 <sebastien.martin1@u-psud.fr>
2017-05-05 15:45:34 +0200

    Add files via upload

---
+++ fonctionsTokenise.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  5 14:34:16 2017
+
+@author: Sébastien
+"""
+
+import re
+
+def normalise(sent, lang):
+    sent = re.sub("\'\'", '"', sent) # two single quotes = double quotes
+    sent = re.sub("[`â€˜â€™]+", r"'", sent) # normalise apostrophes/single quotes
+    sent = re.sub("[â‰ªâ‰«â€œâ€]", '"', sent) # normalise double quotes
+
+    if lang=="en":
+        sent = re.sub("([a-z]{3,})or", r"\1our", sent) # replace ..or words with ..our words (American versus British)
+        sent = re.sub("([a-z]{2,})iz([eai])", r"\1is\2", sent) # replace ize with ise (..ise, ...isation, ..ising)
+    if lang=="fr":
+        replacements = [("keske", "qu' est -ce que"), ("estke", "est -ce que"), ("bcp", "beaucoup")] # etc.
+        for (original, replacement) in replacements:
+            sent = re.sub("(^| )"+original+"( |$)", r"\1"+replacement+r"\2", sent)
+    return sent
+    
+def tokenise_en(sent):
+
+    # deal with apostrophes
+    sent = re.sub("([^ ])\'", r"\1 '", sent) # separate apostrophe from preceding word by a space if no space to left
+    sent = re.sub(" \'", r" ' ", sent) # separate apostrophe from following word if a space if left
+    cannot_precede = ["M", "Prof", "Sgt", "Lt", "Ltd", "co", "etc", "[A-Z]", "[Ii].e", "[eE].g"] #non-exhaustive list
+    regex_cannot_precede = "(?:(?<!"+")(?<!".join(cannot_precede)+"))" 
+    
+    sent = re.sub(regex_cannot_precede+"([\.\,\;\:\)\(\"\?\!]( |$))", r" \1", sent)
+    sent = re.sub("((^| )[\.\?\!]) ([\.\?\!]( |$))", r"\1\2", sent) 
+
+    sent = sent.split() # split on whitespace
+    return sent
+
+def tokenise(sent, lang):
+    if lang=="en":
+        return tokenise_en(sent)
+    elif lang=="fr":
+        return tokenise_fr(sent)
+    else:
+        exit("Lang: "+str(lang)+" not recognised for tokenisation.\n")
+        
+def normalise(sent, lang):
+    sent = re.sub("\'\'", '"', sent) # two single quotes = double quotes
+    sent = re.sub("[`â€˜â€™]+", r"'", sent) # normalise apostrophes/single quotes
+    sent = re.sub("[â‰ªâ‰«â€œâ€]", '"', sent) # normalise double quotes
+    if lang=="en":
+        sent = re.sub("([a-z]{3,})or", r"\1our", sent) # replace ..or words with ..our words (American versus British)
+        sent = re.sub("([a-z]{2,})iz([eai])", r"\1is\2", sent) # replace ize with ise (..ise, ...isation, ..ising)
+    if lang=="fr":
+        replacements = [("keske", "qu' est -ce que"), ("estke", "est -ce que"), ("bcp", "beaucoup")] # etc.
+        for (original, replacement) in replacements:
+            sent = re.sub("(^| )"+original+"( |$)", r"\1"+replacement+r"\2", sent)
+    return sent
+
+def segment_into_sents(paragraph):
+    
+    paragraph = paragraph.replace('\\n','')
+    paragraph = paragraph.replace('\\','')
+
+    cannot_precede = ["M", "Prof", "Sgt", "Lt", "Ltd", "co", "etc", "[A-Z]", "[Ii].e", "[eE].g"] # non-exhaustive list
+    regex_cannot_precede = "(?:(?<!"+")(?<!".join(cannot_precede)+"))"
+    
+    if "\n" in paragraph: exit("Error in paragraph: paragraph contains \n.")       
+    newline_separated = re.sub(regex_cannot_precede+"([\.\!\?]+([\'\â€™\"\)]*( |$)| [\'\â€™\"\) ]*))", r"\1\n", paragraph)
+    sents = newline_separated.strip().split("\n")
+    for s, sent in enumerate(sents):
+        sents[s] = sent.strip()
+    return sents
+    
--- projetParsage.py
+++ projetParsage.py
@@ -6,58 +6,11 @@ Created on Thu May  4 21:52:58 2017
 """
 
 import urllib
+import fonctionsTokenise as ftok
 import re
 from bs4 import BeautifulSoup
 
-            
-def normalise(sent, lang):
-    sent = re.sub("\'\'", '"', sent) # two single quotes = double quotes
-    sent = re.sub("[`â€˜â€™]+", r"'", sent) # normalise apostrophes/single quotes
-    sent = re.sub("[â‰ªâ‰«â€œâ€]", '"', sent) # normalise double quotes
 
-    if lang=="en":
-        sent = re.sub("([a-z]{3,})or", r"\1our", sent) # replace ..or words with ..our words (American versus British)
-        sent = re.sub("([a-z]{2,})iz([eai])", r"\1is\2", sent) # replace ize with ise (..ise, ...isation, ..ising)
-    if lang=="fr":
-        replacements = [("keske", "qu' est -ce que"), ("estke", "est -ce que"), ("bcp", "beaucoup")] # etc.
-        for (original, replacement) in replacements:
-            sent = re.sub("(^| )"+original+"( |$)", r"\1"+replacement+r"\2", sent)
-    return sent
-    
-def tokenise_en(sent):
-
-    # deal with apostrophes
-    sent = re.sub("([^ ])\'", r"\1 '", sent) # separate apostrophe from preceding word by a space if no space to left
-    sent = re.sub(" \'", r" ' ", sent) # separate apostrophe from following word if a space if left
-    cannot_precede = ["M", "Prof", "Sgt", "Lt", "Ltd", "co", "etc", "[A-Z]", "[Ii].e", "[eE].g"] #non-exhaustive list
-    regex_cannot_precede = "(?:(?<!"+")(?<!".join(cannot_precede)+"))" 
-    
-    sent = re.sub(regex_cannot_precede+"([\.\,\;\:\)\(\"\?\!]( |$))", r" \1", sent)
-    sent = re.sub("((^| )[\.\?\!]) ([\.\?\!]( |$))", r"\1\2", sent) 
-
-    sent = sent.split() # split on whitespace
-    return sent
-
-def tokenise(sent, lang):
-    if lang=="en":
-        return tokenise_en(sent)
-    elif lang=="fr":
-        return tokenise_fr(sent)
-    else:
-        exit("Lang: "+str(lang)+" not recognised for tokenisation.\n")
-        
-def normalise(sent, lang):
-    sent = re.sub("\'\'", '"', sent) # two single quotes = double quotes
-    sent = re.sub("[`â€˜â€™]+", r"'", sent) # normalise apostrophes/single quotes
-    sent = re.sub("[â‰ªâ‰«â€œâ€]", '"', sent) # normalise double quotes
-    if lang=="en":
-        sent = re.sub("([a-z]{3,})or", r"\1our", sent) # replace ..or words with ..our words (American versus British)
-        sent = re.sub("([a-z]{2,})iz([eai])", r"\1is\2", sent) # replace ize with ise (..ise, ...isation, ..ising)
-    if lang=="fr":
-        replacements = [("keske", "qu' est -ce que"), ("estke", "est -ce que"), ("bcp", "beaucoup")] # etc.
-        for (original, replacement) in replacements:
-            sent = re.sub("(^| )"+original+"( |$)", r"\1"+replacement+r"\2", sent)
-    return sent
 
 
 def chargement(htmlsrc):
@@ -79,12 +32,14 @@ def chargement(htmlsrc):
 
 def biblioMotPositif():
     # verbes du premier groupe positifs
-    verbePosPrem = ['aimer','adorer','interesser','divertir','sympa']
+    verbePosPrem = ['aimer','adorer','interesser','innover']
     # liste d'adjectifs positifs
-    adjs = ['honorable','agréable','interessant','spectaculaire','émouvant','bien','bon','bonne','heureux']
+    adjs = ['honorable','agréable','spectaculaire','émouvant','bien','bon','bonne','heureux','sympa','super']
+    autres = ['divertir']
     motsPos = []
     motsPos.append(verbePosPrem)
     motsPos.append(adjs)
+    motsPos.append(autres)
     return motsPos
     
 def biblioMotNegatif():
@@ -107,29 +62,34 @@ def negationDansPhrase(phrase):
     negainv = ['pas','trop','sans']
     negavar = ['aucun']
     for j in range(0,len(negainv)), k in range(0,len(negavar)):
-        for i in range(0,len(phrase)):
-            if(negainv[j]==phrase[i] or negavar[k]==phrase[i]):
+            if(negainv[j] in phrase or negavar[k] in phrase[i]):
                 return True
+    
     return False
 #aucune deception sans innovation
 # expr positif vaut le coup
 
 # --------------fin bibliotheque --------------------#
 
-def segment_into_sents(paragraph):
+def containVerbePrem(verbe, phrase):
+    verbe = verbe[:-2] #enleve les 2 derniers caractères
+    regex = re.compile(".*? "+verbe+"(e|es|ent|ons|ez|ent).*?", re.I)
+    phr = ""
+    for p in phrase:
+        phr = phr+p+' '
+    match_obj = re.match(regex, phr)
+    # what is captured by the brackets
+#    mot = match_obj.string
+#    preceding = match_obj.group(1)
+#    suffix = match_obj.group(2)
+    if match_obj!= None:
+        print("contains the verb ’"+verbe+"er"+"’ in the present tense")
 
-    cannot_precede = ["M", "Prof", "Sgt", "Lt", "Ltd", "co", "etc", "[A-Z]", "[Ii].e", "[eE].g"] # non-exhaustive list
-    regex_cannot_precede = "(?:(?<!"+")(?<!".join(cannot_precede)+"))"
-    
-    if "\n" in paragraph: exit("Error in paragraph: paragraph contains \n.")       
-    newline_separated = re.sub(regex_cannot_precede+"([\.\!\?]+([\'\â€™\"\)]*( |$)| [\'\â€™\"\) ]*))", r"\1\n", paragraph)
-    sents = newline_separated.strip().split("\n")
-    for s, sent in enumerate(sents):
-        sents[s] = sent.strip()
-    return sents
-
-def testComment():
-    far = []
+def testComment(comment):
+    biblioMotPositif();
+    # test negation
+    for phrase  in enumerate (comment):
+        phrase
             
 if __name__=="__main__":
     
@@ -142,19 +102,18 @@ if __name__=="__main__":
     comments, notes = chargement(src)
     
     commentsTok = []
-    for c in enumerate (comments): 
+    for c in enumerate(comments): 
         cstr = str(c)
-        cstr = cstr.replace('\\n','')
-        cstr = cstr.replace('\\','')
-        phrases = segment_into_sents(cstr)
+        phrases = ftok.segment_into_sents(cstr)
         sentsTok = []
         for i in range(0,len(phrases)):   
-            phrases[i] = normalise(phrases[i],"fr")
-            phrases[i] = tokenise(phrases[i],"en")
+            phrases[i] = ftok.normalise(phrases[i],"fr")
+            phrases[i] = ftok.tokenise(phrases[i],"en")
             sentsTok.append(phrases[i])
         commentsTok.append(sentsTok)
-        print(sentsTok)
-        print("coment ",commentsTok[0])
-        print("phrase",commentsTok[0][0] )
-        print("token",commentsTok[0][0][0] )
+
+    for comment in commentsTok:
+        for phrase in comment:
+            containVerbePrem('aller',phrase)
+        
     
